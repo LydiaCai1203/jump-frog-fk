b88ac0397a3abd351f3c98b80bb315f691257dc0
LydiaMuaCai <qingjing.cai@chaitin.com>
2025-07-23 18:49:33 +0800

    feat(init): init a swift app

---
+++ jump-frog-fk/jump-frog-fkUITests/jump_frog_fkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  jump_frog_fkUITestsLaunchTests.swift
+//  jump-frog-fkUITests
+//
+//  Created by caiqj on 2025/7/23.
+//
+
+import XCTest
+
+final class jump_frog_fkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
